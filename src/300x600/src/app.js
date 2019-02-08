4aedc7d5fe51d252ecb13c1a3a36b43abdd317cf
Wallef Antoine <antoine.wallef@gmail.com>
2019-02-08 10:43:41 +0100

    more

--- src/300x600/src/app.js
+++ src/300x600/src/app.js
@@ -3,25 +3,51 @@ document.addEventListener("DOMContentLoaded", function()
   //click setup
   //document.getElementById("taparea").addEventListener("click", function(evt){ window.open(window.clickTag) })
 
+  /*
+  var action = new TimelineLite({delay:1})
+    .set(".wrapper",{autoAlpha:1})
+  .set("#wave02",{rotationY:180}) // wave countered
+    .to(".one",3, {morphSVG:".two", repeat:10, yoyo:true, repeatDelay:0.1, ease:Sine.easeInOut })
+    */
 
   //setup
   var timeLine = new TimelineMax(),
   time = 0
 
-  var bg1 = '#bg1'
+  var
+  bg1 = '#bg1',
+  i1 = '#img1',
+  i2 = '#img2',
+  i3 = '#img3',
+  i4 = '#img4'
+
+
+  //timeLine
+  //.set(".wrapper",{autoAlpha:1})
+  //.set("#wave02",{rotationY:180}) // wave countered
+  //.to(".one",3, {morphSVG:".two", repeat:10, yoyo:true, repeatDelay:0.1, ease:Sine.easeInOut })
 
   //init
-  timeLine.set([bg1],{opacity:0},time)
+  timeLine.set([i2,i3,i4],{opacity:0},time)
 
   //screen 1
-  timeLine.set(bg1,{opacity:1},time)
 
-  //screen 2
+
   time = 2
-  timeLine.set(bg1,{opacity:1},time)
+  timeLine.to(i2,1,{opacity:1},time)
+
+  time = 4
+  timeLine.to(i3,1,{opacity:1},time)
+
+  time = 6
+  timeLine.to(i4,1,{opacity:1},time)
+
+  time = 8
+  timeLine.set([i2,i3],{opacity:0},time)
+  timeLine.to(i4,1,{opacity:0},time)
 
   //trigger and replay
-  timeLine.play(0)
+  timeLine.play()
   timeLine.repeat(2)
 
 });
